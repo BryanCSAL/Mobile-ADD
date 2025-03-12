9e85d1c2e35552153d8e07bd46a62bd469f0152a
BCSAL <bryan.c.s.a.lopes@hotmail.com>
2025-03-11 23:11:30 -0300

    Adicionando arquivos

---
+++ App.js
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+
+import Button from './components/button';
+
+var thisNum = 0;
+
+export default function App() {
+  const [num, setNum] = useState(null); // Estado para armazenar o número gerado
+
+  const handlePress = () => {
+     thisNum = thisNum + 1;
+    setNum(thisNum);
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', marginTop: 50 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>
+        {num !== null ? num : 'Pressione o botão'}
+      </Text>
+      
+      <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 30 }}>
+        <Button onPress={handlePress} />
+      </View>
+    </View>
+  );
+}
